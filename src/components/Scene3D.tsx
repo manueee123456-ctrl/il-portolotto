cb4114bba30a3a41fe96d8e4b65e75c1007b744b
manueee123456-ctrl <manueee123456@gmail.com>
2026-09-01 01:44:10 +0200

    Update Scene3D component image source
    Removed base URL handling and updated image source to a Cloudinary URL.

--- src/components/Scene3D.tsx
+++ src/components/Scene3D.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 
 export const Scene3D: React.FC = () => {
-  // Recupera il percorso base per funzionare anche su GitHub Pages
-  const baseUrl = import.meta.env.BASE_URL || '/';
-
   return (
     <div 
       style={{ 
@@ -14,7 +11,7 @@ export const Scene3D: React.FC = () => {
       }}
     >
       <img
-        src={`${baseUrl}barca.webp`}
+        src="https://res.cloudinary.com/clickandboat-production/image/fetch/q_auto:eco/f_auto/c_auto,g_auto,w_auto:100:700/http://prod-clickandboat-public-bucket.s3.eu-central-1.amazonaws.com/activities/product_168335/images/7fca250f8b5d1a353ea58185f988983eecd60d4637a5f5ecd3830f23d5ef3a54.jpg"
         alt="Barca nel porto"
         style={{
           width: '100%',
